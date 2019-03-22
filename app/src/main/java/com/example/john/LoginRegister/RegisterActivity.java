522280a68e6a70fb584705ac3bbfde662b2ee923
coding <1175485381@qq.com>
2019-03-22 10:21:16 +0800

    bug

--- app/src/main/java/com/example/john/LoginRegister/RegisterActivity.java
+++ app/src/main/java/com/example/john/LoginRegister/RegisterActivity.java
@@ -121,7 +121,6 @@ public class RegisterActivity extends AppCompatActivity implements ProgressGener
         }
     }
     public static boolean checkMobile(String mobile) {
-        // "[1]"代表第1位为数字1，"[358]"代表第二位可以为3、5、8中的一个，"\\d{9}"代表后面是可以是0～9的数字，有9位。
         String regex = "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\\d{8}$";
         Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
         Matcher m = p.matcher(mobile);
